77144b28b5d91dfab2503e076c3ebbfe6b46a938
AarushSarda <77834111+AarushSarda@users.noreply.github.com>
2021-05-04 13:02:37 +0530

    Add files via upload

---
+++ room.js
@@ -0,0 +1,12 @@
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyAuTfTIW7OTf-IwWdw5OFy03fx47CSf6AQ",
+    authDomain: "lets-chat-web-app-d6773.firebaseapp.com",
+    databaseURL: "https://lets-chat-web-app-d6773-default-rtdb.firebaseio.com",
+    projectId: "lets-chat-web-app-d6773",
+    storageBucket: "lets-chat-web-app-d6773.appspot.com",
+    messagingSenderId: "672309872235",
+    appId: "1:672309872235:web:f4a1466f93f9b1b8a3532e"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
